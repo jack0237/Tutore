11634d7d10b0cf041aee3d8e7d225a8ef42df14a
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-02-27 19:27:52 +0100

    correction of interfaces

---
+++ script.js
@@ -0,0 +1,9 @@
+function openForm() {
+    document.getElementById("popupForm").style.display = "block";
+  }
+  function closeForm() {
+    document.getElementById("popupForm").style.display = "none";
+  }
+  
+
+  
